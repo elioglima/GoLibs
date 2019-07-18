b09bbe81af07c78052b770360f90c864ff81d1a7
elio.lima <diretoria@maxtime.info>
2019-07-17 21:37:04 -0300

    elio.lima

--- docs.go
+++ docs.go
@@ -2,34 +2,66 @@ package GoLibs
 
 import (
 	"errors"
-	"regexp"
 
 	brdoc "gopkg.in/Nhanderu/brdoc.v2"
 )
 
-func IsCPF(doc string) error {
-	reg, err := regexp.Compile("[^0-9]+")
+func IsCPF(value string) error {
+	processedString, err := SoNumeros(value)
 	if err != nil {
 		return err
 	}
-	processedString := reg.ReplaceAllString(doc, "")
+
 	if ok := brdoc.IsCPF(processedString); !ok {
 		return errors.New("CPF inválido")
 	}
 	return nil
 }
 
-func IsCNPJ(doc string) error {
-	reg, err := regexp.Compile("[^0-9]+")
+func IsCNPJ(value string) error {
+	processedString, err := SoNumeros(value)
 	if err != nil {
 		return err
 	}
-
-	processedString := reg.ReplaceAllString(doc, "")
 
 	if ok := brdoc.IsCNPJ(processedString); !ok {
 		return errors.New("CCNPJ inválido")
 	}
 
 	return nil
+}
+
+func ImprimeCPF(value string) (string, error) {
+
+	processedString, err := SoNumeros(value)
+	if err != nil {
+		return "", err
+	}
+
+	if err := IsCPF(processedString); err != nil {
+		return "", err
+	}
+
+	CPFFormatado := processedString[0:3]
+	CPFFormatado += "." + processedString[3:6]
+	CPFFormatado += "." + processedString[6:9]
+	CPFFormatado += "-" + processedString[9:11]
+	return CPFFormatado, nil
+}
+
+func ImprimeCNPJ(value string) (string, error) {
+	processedString, err := SoNumeros(value)
+	if err != nil {
+		return "", err
+	}
+
+	if err := IsCPF(processedString); err != nil {
+		return "", err
+	}
+
+	CPFFormatado := processedString[0:3]
+	CPFFormatado += "." + processedString[3:6]
+	CPFFormatado += "." + processedString[6:9]
+	CPFFormatado += "-" + processedString[9:11]
+	return CPFFormatado, nil
 }
